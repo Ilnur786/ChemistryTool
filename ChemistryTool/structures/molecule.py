e7edb150c190894748178381a7493acc90c83ea7
Ilnur786 <Ilnur-qwerty@mail.ru>
2020-04-16 23:53:50 +0300

    добавил методы add_atom, add_bond, __repr__. добавил __setitem__ , __repr__

--- ChemistryTool/structures/molecule.py
+++ ChemistryTool/structures/molecule.py
@@ -4,10 +4,29 @@ from ..algorithms import Isomorphism
 
 class Molecule(Isomorphism, MoleculeABC):
     def add_atom(self, element: str, number: int):
-        ...
+        if isinstance(element, str):
+            self._atoms[number] = element
+            self._bonds[number] = {}
+        else:
+            raise TypeError('нужно ввести строку')
 
     def add_bond(self, start_atom: int, end_atom: int, bond_type: int):
-        ...
+        if isinstance(start_atom, int) and isinstance(start_atom, int) and isinstance(start_atom, int):
+            if start_atom == end_atom:
+                raise KeyError('атом не может быть связан с сами собой')
+            elif start_atom not in self._atoms or end_atom not in self._atoms:
+                raise KeyError('таких атомов нет в словаре self._atoms')
+            elif end_atom in self._bonds[start_atom]:  #проверка на то что существует ли связь
+                raise KeyError('такая связь уже есть')
+            else:
+                self._bonds[start_atom].update({end_atom: bond_type})
+                self._bonds[end_atom].update({start_atom: bond_type})
+        else:
+            TypeError('не числа')
+
+    def __repr__(self):
+        return 'Molecule: atoms{}, bonds{}'.format(self._atoms, self._bonds)
 
 
 __all__ = ['Molecule']
+
--- ChemistryTool/structures/reaction.py
+++ ChemistryTool/structures/reaction.py
@@ -17,7 +17,13 @@ class MoleculeList(MoleculeListABC):
         return self._data[i]
 
     def __setitem__(self, i, molecule):
-        pass  # todo: homework!
+        if isinstance(molecule, Molecule):
+            self._data[i] = molecule
+        else:
+            raise TypeError
+
+    def __repr__(self):
+        return 'MoleculeList = {}'.format(self._data)
 
 
 class Reaction(ReactionABC):
